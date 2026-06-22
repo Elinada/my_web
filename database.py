28e25312ae0ad85b74d430f21469794416027cbb
narelita <nareli.cruz@gmail.com>
2026-06-22 13:35:09 +0400

    Database mudular

--- database.py
+++ database.py
@@ -1,19 +1,63 @@
 import sqlite3
-def get_connection():
-    conn = sqlite3.connect("books.db")
-    conn.row_factory = sqlite3.Row
-    return conn
+
+def get_all_books():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, author FROM books")
+    books = cursor.fetchall()
+    conn.close()
+    return books
+
 
 def init_db():
     conn = get_connection()
-    conn.execute(""
+    cursor = conn.cursor()
+    conn.execute('''
         CREATE TABLE IF NOT EXISTS books (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             author TEXT NOT NULL
         )
-    "")
+    ''')
     conn.commit()
     conn.close()
-    
-init_db()
+
+def get_connection():
+    conn = sqlite3.connect("books.db")
+    #conn.row_factory = sqlite3.Row
+    return conn
+
+def insert_book(title, author):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (title, author))  
+        conn.commit()
+        conn.close()
+
+
+def delete_books(id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM books WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+
+def editing_book(id, title, author):
+    conn = get_connection()
+    cursor = conn.cursor()
+    #book = cursor.fetchone()
+    cursor.execute("UPDATE books SET title = ?, author = ? WHERE id = ?", (title, author, id))
+    conn.commit()
+    conn.close()
+
+
+def get_book_id(id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM books WHERE id = ?",
+        (id,)
+    )
+    book = cursor.fetchone()
+    conn.close()
+    return book
--- p1.py
+++ p1.py
@@ -1,35 +1,27 @@
 from flask import Flask, render_template, request, url_for, redirect
-import sqlite3
+from database import get_connection, init_db, insert_book, delete_books, editing_book,get_all_books, get_book_id
 
 
 app = Flask(__name__) 
-
-# create database and table
-def init_db():
-    conn = sqlite3.connect("books.db")
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS books (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   title TEXT NOT NULL,
-                   author TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
-# initialize the database
+   
 init_db()
 
-
+# it does not need to be specified that the method is GET because it is the default method
 @app.route('/')
 def home():
-    conn = sqlite3.connect("books.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, title, author FROM books")
-    books = cursor.fetchall()
-    conn.close()
-
+    books= get_all_books()
     return render_template("index.html", books=books)
+
+@app.route('/edit/<int:id>', methods =['GET', 'POST'])
+def edit_book(id):
+    book = get_book_id(id)
+    # next line verifies if the user is trying to edit a book that does not exist, if so it redirects to the home page
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        editing_book(id, title, author)
+        return redirect(url_for('home'))
+    return render_template("edit.html", book=book)  #this runs only in a GET request
 
 
 @app.route('/books', methods=['GET', 'POST'])
@@ -39,44 +31,17 @@ def books():
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
-        conn = sqlite3.connect("books.db")
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (title, author))  
-        conn.commit()
-        conn.close()
+        insert_book(title, author)
         return redirect(url_for('home'))
     return render_template("books.html",title=title,author=author)
 
+
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_book(id):
-    conn = sqlite3.connect("books.db")
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM books WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
-    #return "Book deleted!"
+    delete_books(id)
     return redirect(url_for('home'))
 
-
-@app.route('/edit/<int:id>', methods =['GET', 'POST'])
-def edit_book(id):
-    conn = sqlite3.connect("books.db")
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT title, author FROM books WHERE id = ?", (id,))
-    book = cursor.fetchone()
-    if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
-        cursor.execute("UPDATE books SET title = ?, author = ? WHERE id = ?", (title, author, id))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('home'))
-    conn.close()
-    return render_template("edit.html", book=book, id=id)
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
   
-
