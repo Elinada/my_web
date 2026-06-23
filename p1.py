02e666071c9e5fcc27c96e4819e75eb5f93fd8a3
narelita <nareli.cruz@gmail.com>
2026-06-23 10:36:04 +0400

    validation and style

--- p1.py
+++ p1.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
-from database import get_connection, init_db, insert_book, delete_books, editing_book,get_all_books, get_book_id
+from flask import Flask, flash, render_template, request, url_for, redirect, abort
+from database import (init_db, 
+                      insert_book, 
+                      delete_books, 
+                      editing_book,get_all_books, get_book_id)
 
 
 app = Flask(__name__) 
@@ -15,11 +18,19 @@ def home():
 @app.route('/edit/<int:id>', methods =['GET', 'POST'])
 def edit_book(id):
     book = get_book_id(id)
+    if book is None:
+        abort(404)  # if the book does not exist, return a 404 error
+    
     # next line verifies if the user is trying to edit a book that does not exist, if so it redirects to the home page
     if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
-        editing_book(id, title, author)
+        title = request.form['title'].strip()
+        author = request.form['author'].strip()
+        
+        if not title or not author:
+            return render_template("edit.html", book=book, error="Title and Author cannot be empty.")
+        
+        editing_book(id, title, author)  #this calls the database manager
+        #flash('Book updated successfully!')
         return redirect(url_for('home'))
     return render_template("edit.html", book=book)  #this runs only in a GET request
 
@@ -29,19 +40,24 @@ def books():
     title = None
     author = None
     if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
+        title = request.form['title'].strip()
+        author = request.form['author'].strip()
+        if not title or not author:
+            return render_template("books.html",title=title,author=author, error="Title and Author cannot be empty.")
+   
         insert_book(title, author)
         return redirect(url_for('home'))
     return render_template("books.html",title=title,author=author)
 
-
+# the only allowed method is POST because we do not want to delete
+#  a book by just clicking a link, we want
+#  to make sure that the user is aware of the action they are taking
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_book(id):
     delete_books(id)
     return redirect(url_for('home'))
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-  
+    # for runing the app in production, use a WSGI server like Gunicorn or uWSGI instead of the built-in Flask server.  
+    #app.run()
